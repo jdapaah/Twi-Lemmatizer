893a659261a8cd59f3b74fb7d26bc9946882fecd
Jeremy Dapaah <jdapaah@princeton.edu>
2023-01-02 19:22:19 -0500

    start building out

--- lemmaV.py
+++ lemmaV.py
@@ -1,85 +1,104 @@
+# Attempt optimazation of memoziation - if known attempt is not a verb, save it
 from vowels import VOWEL_SML_O as o#ɔ
 from vowels import VOWEL_SML_E as e#ɛ
 from vowels import VOWEL_CAP_E as E#Ɛ
 from vowels import VOWEL_CAP_O as O#Ɔ
+from tenses import tenses
+
+from translate import translate as attemptTranslate
 
 complement_word_ending = ['i'+e, 'e'+e]
 irregular_verbs = {"nni": 'w'+o, 'mfa': 'de'}
 
+# return true if the word actually exist in Twi
+# proto word needs to be at least two characters long
+
+def filter(words):
+    new_list = []
+    for wordObj in words:
+        word = wordObj["Root"]
+        attempt = attemptTranslate([word])
+        if attempt != word: # if twi translation to english does not fail
+            wordObj["English"] = attempt
+            new_list.append(wordObj) # is an actual lemmatized verb, add to list
+    return new_list
+
 def stdN(word: str):
     # remove the n prefix and guess the root word
     if word in irregular_verbs:
-        return irregular_verbs[word]
+        return [irregular_verbs[word]]
     ret = []
     if word[:2] == 'mm': # check m or b
         for proto in ['m'+word[2:], 'b'+word[2:]]:
-            if existence(proto):
-                ret.append(proto)
+            # if existence(proto):
+            ret.append(proto)
     elif word[:2] == 'nn': # check n or d
-         for proto in ['n'+word[2:], 'd'+word[2:]]:
-            if existence(proto):
-                ret.append(proto)
+        for proto in ['n'+word[2:], 'd'+word[2:]]:
+            # if existence(proto):
+            ret.append(proto)
     else:
         proto = word[1:]
-        if existence(proto): # standard word
-            ret.append(proto)     
-    return tuple(ret)
+        # if existence(proto): # standard word
+        ret.append(proto)     
+    return ret
 
 
-# return true if the word actually exist in Twi
-# proto word needs to be at least two characters long
-def existence(word):
-    if word and len(word)>=2:
-        return True
-    return False
-
+# return if the word can be a possible negation prefix
 def negationPrefix(word: str):
     if word[:2] in ['mm', 'mp', 'mf']:
         return True
     if word[0] == 'n':
         return True
     return False
-# return if the word can be a possible negation prefix
 
-def lemmatize(word: str):
+"""Lemmatize the given word without context, assuming the part of speech is a verb.
+   This function operates by lemmatizing the word in all manners possible, and then
+   sending a request to Google Translate to attempt to translate the proto-word 
+   into English. If the attempted translation exists as a word in the English 
+   language, it is returned to the client."""
+
+def lemmatizeVerb(word: str):
     results = []
     # presperfN w comp
     if negationPrefix(word) and word[-1]==word[-2]: # n..xx
-        results.append(stdN(word[:-1])+('PRP_N',))
+        for protoRoot in stdN(word[:-1]):
+            results.append({"Twi": word, "Root": protoRoot, "Tense": 'PRP_N'})
     # presPerfN wo comp
-    elif negationPrefix(word) and word[-2:] in complement_word_ending: # n...ie
-        results.append(stdN(word[:-2])+('PRP_N',))
+    if negationPrefix(word) and word[-2:] in complement_word_ending: # n...ie
+        for protoRoot in stdN(word[:-2]):
+            results.append({"Twi": word, "Root": protoRoot, "Tense": 'PRP_N'})
     # futureN, presentN
-    elif negationPrefix(word): #n..
-        results.append(stdN(word)+('PRS_N/FUT_N',))
-    # def pastN(word: str):
-    elif word[0] == 'a' and negationPrefix(word[1:]): # an..
-        results.append(stdN(word[1:])+('PST_N',))
+    if negationPrefix(word): #n..
+        for protoRoot in stdN(word):
+            results.append({"Twi": word, "Root": protoRoot, "Tense": 'PRS_N/FUT_N'})
+    # pastN
+    if word[0] == 'a' and negationPrefix(word[1:]): # an..
+        for protoRoot in stdN(word[1:]):
+            results.append({"Twi": word, "Root": protoRoot, "Tense":'PST_N'})
     # progressiveN_immFutureN
-    elif word[:2] == 're' and negationPrefix(word[2:]): #ren..
-        results.append(stdN(word[2:])+('PRG_N/IMF_N',))
+    if word[:2] == 're' and negationPrefix(word[2:]): #ren..
+        for protoRoot in stdN(word[2:]):
+            results.append({"Twi": word, "Root": protoRoot, "Tense":'PRG_N/IMF_N'})
     # progressive
-    elif word[:2] == 're': #re..
-        results.append((word[2:],'PRG'))
+    if word[:2] == 're': #re..
+        results.append({"Twi": word, "Root": word[2:], "Tense": 'PRG'})
     # future
-    elif word[:2] == 'bɛ': # be..
-        results.append((word[2:],'FUT'))
+    if word[:2] == 'bɛ': # be..
+        results.append({"Twi": word, "Root": word[2:], "Tense": 'FUT'})
     # immFuture
-    elif word[:4] == 'rebɛ': # rebɛ..
-        results.append((word[4:],'IMF'))
+    if word[:4] == 'rebɛ': # rebɛ..
+        results.append({"Twi": word, "Root": word[4:], "Tense": 'IMF'})
     # presPerf
-    elif word[0] == 'a' and len(word)>3: # a..
-        results.append((word[1:],'PRP'))
-    # def past w comp
-    elif word[-2:] in complement_word_ending: #..ie
-        results.append((word[:-2],'PST'))
-    #past wo comp
-    elif word[-1]==word[-2]: #  ..xx
-        results.append((word[:-1],'PST'))
-    # else: # present 
-    #     results.append((word,'PRS'))
-    return results # filter against dictionary
+    if word[0] == 'a' and len(word)>3: # a..
+        results.append({"Twi": word, "Root": word[1:], "Tense": 'PRP'})
+    # past w comp [..ie] or past wo comp ..xx
+    if word[-2:] in complement_word_ending or \
+       word[-1]==word[-2]: #  
+        results.append({"Twi": word, "Root": word[:-2], "Tense": 'PST'})
+    results.append({"Twi": word, "Root": word, "Tense": 'PRS'})
+    
+    return filter(results) # filter against dictionary
 
 if __name__ == '__main__':
     while 1:
-        print(lemmatize(input()))
+        print(lemmatizeVerb(input()))
--- pos-tagger.py
+++ pos-tagger.py
@@ -2,10 +2,12 @@ from vowels import VOWEL_SML_O as o#ɔ
 from vowels import VOWEL_SML_E as e#ɛ
 from vowels import VOWEL_CAP_E as E#Ɛ
 from vowels import VOWEL_CAP_O as O#Ɔ
-from lemmaV import lemmatize
+from lemmaV import lemmatizeVerb
 
 from string import punctuation
 from sys import argv
+import os
+
 """
 PRS: Present, 
 FUT: Future,
@@ -14,16 +16,60 @@ IMF: Immediate Future,
 PST: Past,
 PRP: Present Perfect
 """
-tenses = ['PRS', 'FUT', 'PRG', 'IMF', 'PST', 'PRP', 'PRS_N/FUT_N', 'PRG_N/IMF_N', 'PST_N', 'PRP_N']
-tenses = dict(zip(tenses, [[] for _ in tenses]))
-# tenses.remove('PRS')
+tenses = {key: [] for key in ['PRS', 'FUT', 'PRG', 'IMF', 'PST', 'PRP',
+                            'PRS_N/FUT_N', 'PRG_N/IMF_N', 'PST_N', 'PRP_N']}
+
+simpleTag = {} # provide a mapping from the detailed parts of speech in English to smaller, simpler set
+for v in ['VB', 'VBP', 'VBZ', 'VBD', 'VBN', 'VBG']:
+    simpleTag[v] = "VERB"
+for n in ['NN', 'NNS', 'NNP',  'NNPS']:
+    simpleTag[n] = "NOUN"
+for j in ['JJ', 'JJR', 'JJS']:
+    simpleTag[j] = "ADJ"
+for a in ['RB', 'RBR', 'RBS']:
+    simpleTag[a] = "ADVERB"
+for p in ['TO', 'IN']:
+    simpleTag[p] = "PREPOS"
+
 # The present tense functions as the default in the lemmatizer
 # so it removed from the statistics to avoid heavily skewing the data
-for line in open(argv[1]):
-    for word in line.translate(str.maketrans('', '', punctuation)).split():
-        results = lemmatize(word.lower()) # list of possible tenses
-        for i in results:
-            tenses[i[-1]] +=list(i[:-1])
+def main():
+    for line in open(argv[1]):
+        for word in line.translate(str.maketrans('', '', punctuation)).split():
+            print("-----")
+            for i in lemmatizeVerb(word.lower()): # list of possible tenses if a verb
+                if createTinyFile(i['English']): # create tiny file and return success code
+                    success, tag = tagTinyFile()
+                    if not success: # something went wrong with the tagging
+                        continue
+                else: # something went wrong with the file creation
+                    continue
+                potentialTag = simpleTag.get(tag, tag)
+                
+                # if tense == prs, it is either an actual present verb or different part of speech entirely
 
-for k, v in tenses.items():
-    print(k, v)
+                if i["Tense"] != "PRS": # some inflection is performed, likely a verb
+                    if i["Tense"] in ['PRS_N/FUT_N', 'PRP_N']: # n..
+                        potentialTag += "_POSSIBLE-PLURAL-NOUN" # could be a plural noun?
+                    elif potentialTag != 'VERB': # if modified but not a verb
+                        continue # ignore, false lemmatization
+                
+                print(word, potentialTag)
+
+"""Create a file that serves as input to the Stanford Part of Speech Tagger.
+    and execute the Stanford Part of Speech Tagger.
+    Return an exit code of false if something fails,
+    as well as the output of the tagger"""
+def tag(word):
+    with open('temp.tag', 'w') as file:
+        file.write(word)
+    exitCode = os.system("cd stanford-postagger-full-2020-11-17; \
+                         ./stanford-postagger.sh models/english-left3words-distsim.tagger ../temp.tag > ../output")
+    if exitCode != 0:
+        return False, ""
+    with open("output") as file:
+        output = file.read()
+        print(output)
+    return True , [output[output.rindex("_")+1:].strip() for output in output.split()]
+
+print(tag("I . am . going . to . run ."))
